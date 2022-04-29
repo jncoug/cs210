9de164331277b1ace88587fe0ddeddbe9036c874
jncoug <jncoug@gmail.com>
2022-04-29 10:51:40 -0600

    added dice game

--- dice/Program.cs
+++ dice/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+void main() {
+    Person person = new Person();
+    Dice dice = new Dice();
+
+}
+
+
+public class Person {
+    public int score = 0;
+    public Person() 
+    {
+    }
+
+    public void addScore() {
+
+    }
+
+}
+
+public class Dice {
+    public List<int> diceValues = new List<int>(5);
+    Random random = new Random();
+    
+
+    public Dice()
+    {
+        for (int i = 0; i < 5; i++ ) {
+            diceValues.Add(0);
+        
+        }
+    }
+
+    public void rollDice() {
+        for (int i = 0; i < 5; i++ ) {
+            int value = random.Next(1,6);
+            diceValues[i] = value;
+        }
+    }
+}
+
+
