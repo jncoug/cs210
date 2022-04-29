7c2778239ffadbaaa0f9fea2a0566b907c082f57
jncoug <jncoug@gmail.com>
2022-04-29 13:04:58 -0600

    Added a Game manager class to our Dice and Person Class to run the game

--- dice/Program.cs
+++ dice/Program.cs
@@ -1,19 +1,126 @@
 ﻿
 void main() {
-    Person person = new Person();
-    Dice dice = new Dice();
 
+    GameManager gm = new GameManager();
+    gm.startGame();
+
+}
+
+main();
+
+public class GameManager {
+
+    List<Person> players = new List<Person>();
+    bool gameOver = false;
+
+
+    public GameManager()
+    {
+    }
+
+    public int getPlayers() {
+
+        Console.WriteLine("How many players would you like?");
+
+        int numPlayers = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= numPlayers; i++) {
+            Console.WriteLine($"Enter name for Player {i}:");
+            string playerName = Console.ReadLine();
+            players.Add(new Person(playerName));
+        }
+
+        return numPlayers;
+    }
+
+    public void startGame() {
+        Console.WriteLine("Welcome to the Dice Game!");
+        Console.WriteLine("*************************");
+        
+        int numPlayers = getPlayers();
+        int playersFinished = 0;
+
+        string input;
+
+        while(!gameOver) {
+
+            foreach(Person player in players) {
+
+                if (player.isPlaying) {
+                    Console.WriteLine($"{player.name}({player.score} points): Would you like to roll the dice? (y/n)");
+                    input = Console.ReadLine();
+                    if (input == "y") {
+                        int points = player.rollDice();
+                        if (points == 0) {
+                            playersFinished++;
+                        }
+                    }
+                    else {
+                        player.endGame();
+                        playersFinished++;
+                    }
+                }
+            }
+
+            if (playersFinished == numPlayers) {
+                    gameOver = true;
+            }
+        }
+        
+        endGame();
+    }
+
+    public void endGame() {
+        string winnerName = "";
+        int winnerScore = 0;
+
+        foreach(Person player in players) {
+            
+            if (player.score > winnerScore) {
+                winnerScore = player.score;
+                winnerName = player.name;
+            }
+        }
+
+        Console.WriteLine($"The winner is {winnerName} with a score of {winnerScore}!");
+    }
 }
 
 
 public class Person {
+
     public int score = 0;
-    public Person() 
+    public bool isPlaying = true;
+    public string name;
+
+    public Person(string playerName) 
     {
+        name = playerName;
     }
 
-    public void addScore() {
+    public int rollDice() {
+        Dice dice = new Dice();
+        int points = dice.rollDice();
+        if (points == 0) {
+            score = 0;
+            endGame();
+        }
+        else {
+            score += points;
+            Console.WriteLine($"Points: {points} Score: {score}");
+        }
 
+        return points;
+    }
+    
+    public void endGame() {
+        if (score == 0) {
+            Console.WriteLine("No Luck! You finished the game with 0 points.");
+        }
+        else {
+            Console.WriteLine($"You finished the game with {score} points.");
+        }
+        isPlaying = false;
     }
 
 }
@@ -31,12 +138,31 @@ public class Dice {
         }
     }
 
-    public void rollDice() {
+    public int rollDice() {
+        int score = 0;
+        string diceRollString = "";
         for (int i = 0; i < 5; i++ ) {
             int value = random.Next(1,6);
             diceValues[i] = value;
         }
+
+        foreach (int value in diceValues) {
+            diceRollString += value.ToString();
+            diceRollString += " ";
+            if (value == 1) {
+                score += 100;
+            }
+            else if (value == 5) {
+                score += 50;
+            }
+        }
+        Console.WriteLine($"You rolled: {diceRollString}");
+
+        return score;
+
     }
 }
+
+
 
 
